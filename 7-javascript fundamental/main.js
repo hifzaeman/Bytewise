021b8321ba959e4a1597687c468e4031c0936ae5
hifzaeman <92920728+hifzaeman@users.noreply.github.com>
2023-03-31 20:13:27 +0500

    Add files via upload

---
+++ 7-javascript fundamental/main.js
@@ -0,0 +1,26 @@
+console.log('Hello from js')
+//variables
+let age=25
+console.log(age)
+const  salary=40000// const variable values cannot be changed
+console.log(salary)
+const oddnumbers=[1,3,5,7,9]
+console.log(oddnumbers[2])
+let a=10
+a='wiswi'
+a=true
+let x=10
+const isValid=true
+console.log('Hifza'+'Eman')
+const isEven=10%2===0
+console.log(parseFloat('5.67'))
+console.log((500).toString())
+console.log(Boolean(10))
+const var1='test'
+const var2='test'
+console.log(var1==var2)//type converted and then compared
+console.log(var1===var2)//type not converted
+const numArray=[1, 2, 3, 4, 5]
+for(const num of numArray)
+{console.log('Iteratio number'+num)}
+
